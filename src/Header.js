4ae860bce5e9f2e74977dc797713ce44c6584d2e
Rifqi Subagja <rifqisubagjaa@gmail.com>
2023-02-15 11:56:14 +0700

    Add component Navbar React

---
+++ src/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <h1>This is react</h1>;
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import Nav from "./Nav";
+import Header from "./Header";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const render = (content, id) => {
+  ReactDOM.render(content, document.getElementById(id));
+};
+render(<Nav />, "nav");
+render(<Header />, "root");
